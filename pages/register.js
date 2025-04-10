fe736595080e5f1281f37dad082078eb4d2d2137
ABINJ007 <abinj7766@gmail.com>
2025-04-11 01:14:28 +0530

    updated

--- pages/register.js
+++ pages/register.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { supabase } from './supabaseClient';
+import { supabase } from '../lib/supabaseClient';
 import styles from '../styles/Register.module.css';
 import Image from 'next/image';
 
